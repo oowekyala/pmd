524b9469de0781a900ff13b63345a7d7498e8a98
Clément Fournier <clement.fournier76@gmail.com>
2020-04-12 14:08:18 +0200

    Migrate java escape reader to block IO

--- pmd-core/src/main/java/net/sourceforge/pmd/lang/ast/impl/JavaInputReader.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/lang/ast/impl/JavaInputReader.java
@@ -4,258 +4,222 @@
 
 package net.sourceforge.pmd.lang.ast.impl;
 
-import java.io.EOFException;
+import static java.lang.Integer.min;
+
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.CharBuffer;
 
 import net.sourceforge.pmd.util.StringUtil;
 
-public class JavaInputReader extends Reader {
+/**
+ * An implementation of java.io.Reader that translates Java unicode escapes.
+ * This implementation has efficient block IO but poor char-by-char performance.
+ */
+@SuppressWarnings("PMD.AssignmentInOperand")
+public final class JavaInputReader extends Reader {
 
     /** Untranslated characters. */
-    private final String input;
-    /** Buffer for translated characters. */
-    private final char[] buffer;
-
-    /** Position in buffer. */
-    private int bufpos = -1;
-    /** Number of characters the {@link #bufpos} is behind the current valid offset in the {@link #buffer}. */
-    private int ahead = 0;
-
+    private char[] input;
+    /** Position of the next char to read in the input. */
+    private int bufpos;
     /** Keep track of adjustments to make to the offsets, caused by unicode escapes. */
-    private EscapeTracker escapes = new EscapeTracker();
-    /** Index of the next char to read in the {@link #input}. */
-    private int idxInInput = 0;
-    /** Mark recorded by {@link #mark(int)}, if negative, no mark is recorded. */
-    private int mark = -1;
+    private final EscapeTracker escapes = new EscapeTracker();
 
-    public JavaInputReader(String input, int bufsize) {
-        assert bufsize > 0;
+    private int savedNotEscapeSpecialEnd = Integer.MAX_VALUE;
+
+    public JavaInputReader(CharSequence input, int startIdxInclusive, int endIdxExclusive) {
         assert input != null;
+        assert startIdxInclusive >= 0;
+        assert endIdxExclusive >= 0;
+        assert endIdxExclusive >= startIdxInclusive;
 
-        this.input = input;
-        this.buffer = new char[bufsize];
+        int len = endIdxExclusive - startIdxInclusive;
+
+        this.input = new char[len];
+        input.toString().getChars(startIdxInclusive, endIdxExclusive, this.input, 0);
+        bufpos = 0;
     }
 
-    private char readByte() throws IOException {
-        if (idxInInput++ >= input.length()) {
-            throw new EOFException();
-        }
-
-        return input.charAt(idxInInput);
+    public JavaInputReader(CharSequence input) {
+        this(input, 0, input.length());
     }
 
-    /** Move the {@link #bufpos} around the {@link #buffer} by the given amount. */
-    private void shift(int amount) {
-        int diff = (bufpos += amount) - buffer.length;
-        if (diff > 0) {
-            bufpos = diff;
-        }
-    }
 
     @Override
-    public int read(char[] cbuf, int off, int len) throws IOException {
-        int read = -1;
-        int i = off;
-        final int end = off + len;
-        while (i < end) {
-            try {
-                cbuf[i] = (char) read();
-            } catch (EOFException eof) {
-                return read;
-            }
-
-            read++;
-            i++;
-        }
-        return read;
-    }
-
-    @Override
-    public int read() throws IOException {
-        shift(1);
-
-        if (ahead-- > 0) {
-            return buffer[bufpos];
+    public int read(final char[] cbuf, final int off, final int len) throws IOException {
+        ensureOpen();
+        if (this.bufpos == input.length) {
+            return -1;
         }
 
-        char c;
+        int readChars = 0;
+        while (readChars < len && this.bufpos < input.length) {
+            int bpos = this.bufpos;
+            int nextJump = gobbleMaxWithoutEscape(bpos, len - readChars);
+            int newlyReadChars = nextJump - bpos;
 
-        final int posOfFirstBackSlash = idxInInput;
-        if ((buffer[bufpos] = c = readByte()) != '\\') {
-            return c;
-        } else { // seen a backslash
+            assert newlyReadChars >= 0 && (readChars + newlyReadChars) <= len;
 
-            int backSlashCnt = 1;
-
-            // Read all the backslashes
-            while ((buffer[bufpos] = c = readByte()) == '\\') {
-                backSlashCnt++;
-                shift(1);
+            if (newlyReadChars != 0) {
+                System.arraycopy(input, bpos, cbuf, off + readChars, newlyReadChars);
+            } else if (nextJump == input.length) {
+                // eof
+                break;
             }
+            readChars += newlyReadChars;
+        }
+        return readChars;
+    }
 
-            if ((c != 'u') || ((backSlashCnt & 1) != 1)) {
-                backup(backSlashCnt);  // go back to the first backslash, keep chars in buffer
-                return '\\';
-            }
 
-            int escapeLen = backSlashCnt;
-            // odd number of backslashes + 'u' -> we're looking at a unicode escape
-            try {
-                while ((c = readByte()) == 'u') {
-                    // read
-                    backSlashCnt++;
-                }
+    /**
+     * Returns the max offset, EXclusive, with which we can cut the input
+     * array from the bufpos to dump it into the output array. This sets
+     * the bufpos to where we should start the next jump.
+     */
+    private int gobbleMaxWithoutEscape(final int bufpos, final int maxReadahead) throws IOException {
+        int off = bufpos;
+        int max = min(bufpos + maxReadahead, input.length);
+        boolean noBackSlash = false;
+        int notEscapeEnd = this.savedNotEscapeSpecialEnd;
+        while (off < max && (noBackSlash = input[off] != '\\' || notEscapeEnd < off)) {
+            off++;
+        }
 
-                shift(1);
+        if (noBackSlash) {
+            this.bufpos = off;
+            return off;
+        }
 
-                buffer[bufpos] = c = (char)
-                    (hexVal(c) << 12 | hexVal(readByte()) << 8 | hexVal(readByte()) << 4 | hexVal(readByte()));
+        final int firstBslashOff = off;
+        while (off < input.length && input[off] == '\\') {
+            off++;
+        }
 
-            } catch (IllegalArgumentException e) {
-                throw new IOException("Invalid escape sequence at " + getLine() + ":" + getColumn(), e);
-            }
+        int bslashCount = off - firstBslashOff;
+        // this condition is "is there an escape at offset firstBslashOff"
+        if ((bslashCount & 1) == 1    // odd number of backslashes
+            && off < input.length - 4 // at least 5 chars to form the escape ('u' + 4 hex digits)
+            && input[off] == 'u') {   // the char after the last backslash is a 'u'
 
-            escapes.recordEscape(posOfFirstBackSlash, escapeLen);
-
-            if (backSlashCnt == 1) {
-                return c;
-            } else {
-                backup(backSlashCnt);
-                return '\\';
-            }
+            replaceFirstBackslashWithEscape(firstBslashOff, off);
+            this.savedNotEscapeSpecialEnd = Integer.MAX_VALUE;
+            this.bufpos = off + 5;
+            this.escapes.recordEscape(firstBslashOff, off + 5 - firstBslashOff);
+            return firstBslashOff + 1;
+        } else {
+            // not an escape sequence
+            int min = min(bufpos + maxReadahead, off);
+            // save the number of backslashes that are part of the escape,
+            // might have been cut in half by the maxReadahead
+            this.savedNotEscapeSpecialEnd = min < off ? off : Integer.MAX_VALUE;
+            this.bufpos = min;
+            return min;
         }
     }
 
-    /** Retreat. */
-    public void backup(int amount) {
-        ahead += amount;
-        shift(-amount);
-    }
+    private void replaceFirstBackslashWithEscape(int posOfFirstBackSlash, int offOfTheU) throws IOException {
+        try {
+            char c = (char)
+                    ( hexVal(input[++offOfTheU]) << 12
+                    | hexVal(input[++offOfTheU]) << 8
+                    | hexVal(input[++offOfTheU]) << 4
+                    | hexVal(input[++offOfTheU])
+                    );
+            input[posOfFirstBackSlash] = c; // replace the start char of the backslash
+        } catch (NumberFormatException e) {
 
-    @Override
-    public void mark(int readAheadLimit) throws IOException {
-        if (bufpos < 0) {
-            throw new IOException("Cannot mark a non-opened stream"); // TODO this is to avoid inconsistencies for now
+            String message = "Invalid escape sequence at line "
+                + getLine(posOfFirstBackSlash) + ", column "
+                + getColumn(posOfFirstBackSlash);
+
+            throw new IOException(message, e);
         }
-        this.mark = bufpos;
-    }
-
-    @Override
-    public boolean markSupported() {
-        return true;
     }
 
     @Override
     public void close() throws IOException {
-        mark = -1;
-        reset();
+        this.bufpos = -1;
+        this.input = null;
+    }
+
+
+    /** Check to make sure that the stream has not been closed */
+    private void ensureOpen() throws IOException {
+        if (input == null) {
+            throw new IOException("Stream closed");
+        }
     }
 
     @Override
-    public void reset() throws IOException {
-        if (mark > 0) {
-            backup(markLength());
-            mark = -1;
-        } else {
-            bufpos = -1;
-            mark = -1;
-            ahead = 0;
-            idxInInput = 0;
-            escapes = new EscapeTracker();
-        }
+    public boolean ready() throws IOException {
+        ensureOpen();
+        return true;
     }
 
-    public int markLength() {
-        if (mark < 0) {
-            throw new IllegalStateException("No recorded mark");
-        } else if (mark <= bufpos) {
-            return bufpos - mark;
-        } else {
-            return (buffer.length - bufpos) + mark;
-        }
+    /**
+     * Returns the offset in the input text of the given translated offset.
+     * This includes the length of any unicode escapes.
+     *
+     * <pre>
+     * input:      "a\u00a0b"
+     * translated: "a b"
+     *
+     * inputOffset(0) = 0
+     * inputOffset(1) = 1
+     * inputOffset(2) = 7 // includes the length of the escape
+     * </pre>
+     */
+    public int inputOffset(int outputOffset) {
+        return escapes.inputOffsetAt(outputOffset);
     }
 
-    public String markImage() {
-        if (mark < 0) {
-            throw new IllegalStateException("No recorded mark");
-        } else if (mark <= bufpos) {
-            return new String(buffer, mark, bufpos - mark + 1);
-        } else {
-            return new String(buffer, mark, buffer.length - mark)
-                + new String(buffer, 0, bufpos + 1);
-        }
+    private int getLine(int idxInInput) {
+        return StringUtil.lineNumberAt(CharBuffer.wrap(input), idxInInput);
     }
 
-    /** Returns the offset of the mark in the input text (inclusive). */
-    public int markOffset() {
-        if (mark < 0) {
-            throw new IllegalStateException("No recorded mark");
-        } else {
-            return escapes.inputOffsetAt(mark);
-        }
-    }
-
-    /** Returns the current offset in the input text (exclusive). */
-    public int curOffset() {
-        return escapes.inputOffsetAt(bufpos) + 1;
-    }
-
-    private int getLine() {
-        int inOffset = this.escapes.inputOffsetAt(bufpos);
-        return StringUtil.lineNumberAt(input, inOffset);
-    }
-
-    private int getColumn() {
-        int inOffset = this.escapes.inputOffsetAt(bufpos);
-        return StringUtil.columnNumberAt(input, inOffset);
+    private int getColumn(int idxInInput) {
+        return StringUtil.columnNumberAt(CharBuffer.wrap(input), idxInInput);
     }
 
     private static int hexVal(char c) {
         switch (c) {
         case '0':
-            return 0;
         case '1':
-            return 1;
         case '2':
-            return 2;
         case '3':
-            return 3;
         case '4':
-            return 4;
         case '5':
-            return 5;
         case '6':
-            return 6;
         case '7':
-            return 7;
         case '8':
-            return 8;
         case '9':
-            return 9;
-        case 'a':
+            return c - '0';
         case 'A':
-            return 10;
-        case 'b':
         case 'B':
-            return 11;
-        case 'c':
         case 'C':
-            return 12;
-        case 'd':
         case 'D':
-            return 13;
-        case 'e':
         case 'E':
-            return 14;
-        case 'f':
         case 'F':
-            return 15;
+            return c - ('A' - 10);
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+            return c - ('a' - 10);
+        default:
+            throw new NumberFormatException("Character '" + c + "' is not a valid hexadecimal digit");
         }
-
-        throw new IllegalArgumentException("Character " + c + " is not a valid hexadecimal digit");
     }
 
+    /**
+     * Records where escapes occurred in the input document. This is quite
+     * an inefficient way to deal with it, yet in the common case where there
+     * are few/no escapes, it's enough I think.
+     */
     private static class EscapeTracker {
 
         private static final int[] EMPTY = new int[0];
@@ -290,29 +254,28 @@ public class JavaInputReader extends Reader {
          * Convert an offset in the translated file into an offset in
          * the untranslated input.
          */
-        int inputOffsetAt(int translatedOffset) {
-            // basically accumulate the lengths of all escapes occurring
-            // before the translated offset
-
-            //
-            // a\u00a0b
-            // a b
-            //
-            //
-            // inputOffsetAt(0) = 0
-            // inputOffsetAt(1) = 1
-            // inputOffsetAt(2) = 7
-            //
-
+        public int inputOffsetAt(int translatedOffset) {
+            // basically accumulate the lengths of all escapes occurring before the given translatedOffset
             int sum = translatedOffset;
             for (int i = 0; i < nextFreeIdx; i += 2) {
-                if (inputOffsets[i] < translatedOffset) {
+                if (inputOffsets[i] < sum) {
                     sum += inputOffsets[i + 1];
                 } else {
                     break;
                 }
             }
             return sum;
+        }
+
+
+        @Override
+        public String toString() {
+            StringBuilder res = new StringBuilder("Escape set {");
+            for (int i = 0; i < nextFreeIdx; i += 2) {
+                res.append("(at=").append(inputOffsets[i]).append(", len=").append(inputOffsets[i + 1]).append("), ");
+            }
+
+            return res.append('}').toString();
         }
     }
 
