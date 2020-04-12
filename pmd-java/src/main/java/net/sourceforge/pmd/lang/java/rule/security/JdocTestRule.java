47c33fb8b9be87ce92ac9cb74821031f9e13444d
Clément Fournier <clement.fournier76@gmail.com>
2020-04-12 12:22:09 +0200

    Make benchmark rule

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/JdocTestRule.java
@@ -0,0 +1,115 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.security;
+
+import java.util.List;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
+import net.sourceforge.pmd.lang.java.ast.JavadocCommentOwner;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+public class JdocTestRule extends AbstractJavaRule {
+
+
+    private static class State {
+
+        public int fileId = 0;
+        public long numResolved = 0;
+        public long numerrors = 0;
+        public long numUnresolved = 0;
+
+        void print() {
+            System.out.println();
+            System.out.println(fileId);
+            System.out.println("Resolved: " + numResolved + ", unresolved " + numUnresolved);
+            double rate = numResolved / (double) (numUnresolved + numResolved);
+            System.out.println("Resolved " + Math.floor(10000 * rate) / 100 + "%");
+            System.out.println("Errors\t" + numerrors);
+            //            System.out.println("CL cache size\t" + ClasspathSymbolResolver.cacheSize);
+            //            System.out.println("CL stream found\t" + ClasspathSymbolResolver.streamFound);
+            //            System.out.println("CL class found\t" + ClasspathSymbolResolver.classFound);
+        }
+
+        int absorb(State other) {
+            fileId += other.fileId;
+            numResolved += other.numResolved;
+            numUnresolved += other.numUnresolved;
+            numerrors += other.numerrors;
+
+            return fileId;
+        }
+
+    }
+
+    private static final State STATIC = new State();
+
+    private State state = new State();
+
+    @Override
+    public void apply(List<? extends Node> nodes, RuleContext ctx) {
+        try {
+            super.apply(nodes, ctx);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    @Override
+    //    public Object visit(ASTCompilationUnit node, Object data) {
+    //        // do nothing
+    //        return data;
+    //    }
+
+    @Override
+    public Object visit(JavaNode node, Object data) {
+
+        if (node instanceof JavadocCommentOwner) {
+            try {
+                ((JavadocCommentOwner) node).getJavadocComment();
+                state.numResolved++;
+            } catch (Throwable e) {
+                System.err.println(position(node, data));
+                e.printStackTrace();
+                state.numerrors++;
+                if (e instanceof Error) {
+                    // throw e;
+                }
+            }
+        }
+
+        return super.visit(node, data);
+    }
+
+    @NonNull
+    public String position(JavaNode node, Object data) {
+        return "In: " + ((RuleContext) data).getSourceCodeFile() + ":" + node.getBeginLine() + ":"
+            + node.getBeginColumn();
+    }
+
+    @Override
+    public void end(RuleContext ctx) {
+        super.end(ctx);
+        state.fileId++;
+        if (state.fileId % 200 == 0) {
+            int fid;
+            synchronized (STATIC) {
+                fid = STATIC.absorb(state);
+            }
+            state = new State();
+
+            if (fid % 400 == 0) {
+                synchronized (STATIC) {
+                    if (STATIC.fileId % 400 == 0) {
+                        STATIC.print();
+                    }
+                }
+            }
+        }
+    }
+}
