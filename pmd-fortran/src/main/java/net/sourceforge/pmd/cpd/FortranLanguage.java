8d9f492ad36af69f3693e46b2dc1483debcdb956
Clément Fournier <clement.fournier76@gmail.com>
2020-08-31 13:18:30 +0200

    Port JS

--- pmd-fortran/src/main/java/net/sourceforge/pmd/cpd/FortranLanguage.java
+++ pmd-fortran/src/main/java/net/sourceforge/pmd/cpd/FortranLanguage.java
@@ -4,16 +4,21 @@
 
 package net.sourceforge.pmd.cpd;
 
+import net.sourceforge.pmd.lang.BaseLanguageModule;
+import net.sourceforge.pmd.lang.CpdOnlyHandler;
+
 /**
  * Language implementation for Fortran
  *
  * @author Romain PELISSE belaran@gmail.com
  */
-public class FortranLanguage extends AbstractLanguage {
+public class FortranLanguage extends BaseLanguageModule {
+
     /**
      * Create a Fortran Language instance.
      */
     public FortranLanguage() {
-        super("Fortran", "fortran", new FortranTokenizer(), ".for", ".f", ".f66", ".f77", ".f90");
+        super("Fortran", "fortran", "fortran", ".for", ".f", ".f66", ".f77", ".f90");
+        addDefaultVersion("", new CpdOnlyHandler(FortranTokenizer::new));
     }
 }
--- pmd-javascript/src/main/java/net/sourceforge/pmd/lang/ecmascript/Ecmascript3Handler.java
+++ pmd-javascript/src/main/java/net/sourceforge/pmd/lang/ecmascript/Ecmascript3Handler.java
@@ -4,6 +4,9 @@
 
 package net.sourceforge.pmd.lang.ecmascript;
 
+import net.sourceforge.pmd.cpd.EcmascriptTokenizer;
+import net.sourceforge.pmd.cpd.Tokenizer;
+import net.sourceforge.pmd.cpd.Tokenizer.CpdProperties;
 import net.sourceforge.pmd.lang.AbstractPmdLanguageVersionHandler;
 import net.sourceforge.pmd.lang.Parser;
 import net.sourceforge.pmd.lang.ParserOptions;
@@ -12,6 +15,11 @@ import net.sourceforge.pmd.lang.ParserOptions;
  * Implementation of LanguageVersionHandler for the ECMAScript Version 3.
  */
 public class Ecmascript3Handler extends AbstractPmdLanguageVersionHandler {
+
+    @Override
+    public Tokenizer getCpdTokenizer(CpdProperties cpdProperties) {
+        return new EcmascriptTokenizer().withProperties(cpdProperties);
+    }
 
     @Override
     public ParserOptions getDefaultParserOptions() {
