9d0344b64f702c7138170699f1e481d281a87d7b
Clément Fournier <clement.fournier76@gmail.com>
2020-08-31 07:08:09 +0200

    Cleanups for antlr tokenizers

--- pmd-kotlin/src/main/java/net/sourceforge/pmd/cpd/KotlinTokenizer.java
+++ pmd-kotlin/src/main/java/net/sourceforge/pmd/cpd/KotlinTokenizer.java
@@ -5,6 +5,7 @@
 package net.sourceforge.pmd.cpd;
 
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Lexer;
 
 import net.sourceforge.pmd.cpd.internal.AntlrTokenizer;
 import net.sourceforge.pmd.cpd.token.AntlrTokenFilter;
@@ -18,9 +19,8 @@ import net.sourceforge.pmd.lang.kotlin.antlr4.Kotlin;
 public class KotlinTokenizer extends AntlrTokenizer {
 
     @Override
-    protected AntlrTokenManager getLexerForSource(SourceCode sourceCode) {
-        CharStream charStream = AntlrTokenizer.getCharStreamFromSourceCode(sourceCode);
-        return new AntlrTokenManager(new Kotlin(charStream), sourceCode.getFileName());
+    protected Lexer getLexerForSource(CharStream charStream) {
+        return new Kotlin(charStream);
     }
 
     @Override
