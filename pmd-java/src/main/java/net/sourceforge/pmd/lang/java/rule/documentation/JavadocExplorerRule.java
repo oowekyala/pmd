209395ecda28c738dbee6c2aef33c1fb20f44819
Clément Fournier <clement.fournier76@gmail.com>
2025-05-26 00:40:13 +0200

    Fix java node first token when there are modifiers

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/documentation/JavadocExplorerRule.java
@@ -0,0 +1,66 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.documentation;
+
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTConstructorDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTEnumDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.ast.JavadocCommentOwner;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.javadoc.ast.JavadocNode;
+import net.sourceforge.pmd.lang.javadoc.ast.JavadocNode.JdocComment;
+
+/**
+ * @author Clément Fournier
+ */
+public class JavadocExplorerRule extends AbstractJavaRule {
+
+    private int numUncommented;
+    private int total;
+
+    @Override
+    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+        process(node, data);
+        return super.visit(node, data);
+    }
+
+    @Override
+    public Object visit(ASTEnumDeclaration node, Object data) {
+        process(node, data);
+        return super.visit(node, data);
+    }
+
+    @Override
+    public Object visit(ASTMethodDeclaration node, Object data) {
+        process(node, data);
+        return super.visit(node, data);
+    }
+
+    @Override
+    public Object visit(ASTConstructorDeclaration node, Object data) {
+        process(node, data);
+        return super.visit(node, data);
+    }
+
+    private void process(JavadocCommentOwner commentOwner, Object data) {
+        total++;
+        JdocComment comment;
+        try {
+            comment = commentOwner.getJavadocComment();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return;
+        }
+        if (comment == null) {
+            numUncommented++;
+            return;
+        }
+
+        System.out.println(((RuleContext) data).getSourceCodeFilename() + ","
+                               + comment.findDescendantsOfType(JavadocNode.class).size());
+    }
+}
