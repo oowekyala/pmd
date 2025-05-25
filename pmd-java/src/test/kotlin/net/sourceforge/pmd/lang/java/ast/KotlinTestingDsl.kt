9cbe2c7c975b65e8ed0afd3a2ef880d19cd02042
Clément Fournier <clement.fournier76@gmail.com>
2025-05-26 00:39:39 +0200

    Fix test

--- pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/KotlinTestingDsl.kt
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/KotlinTestingDsl.kt
@@ -86,6 +86,7 @@ object CustomTreePrinter : KotlintestBeanTreePrinter<Node>(NodeTreeLikeAdapter) 
             when {
                 prop.name in ignoredProps                          -> false
                 prop.readMethod?.declaringClass !== node.javaClass -> false
+
                 // avoid outputting too much, it's bad for readability
                 node is ASTNumericLiteral                          -> when {
                     node.isIntegral -> prop.name == "valueAsInt"
